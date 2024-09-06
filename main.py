ed9c4539619daff0a2dcf0af4ed7503542a25ec4
Vladimir Sorokin <bcc20080913@yandex.ru>
2024-09-06 22:50:49 +1000

    0.0.3

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ def test_login(ddriver):
     driver = ddriver
     driver.get(Where.main_pass)
     print('\n',Locator.locator1[0],Locator.locator1[1])
-    #elm = driver.find_element(Locator.locator1[0],Locator.locator1[1])
+    elms = driver.find_elements(Locator.locator1[0],Locator.locator1[1])
+    for elm in elms:
+        print(elm.get_attribute('class'))
     print(driver.current_url)
     assert 'irisqul' in driver.current_url
 
