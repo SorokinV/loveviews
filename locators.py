da745de99fe248e0984de7f34f354cd405c63cbc
Vladimir Sorokin <bcc20080913@yandex.ru>
2024-09-06 23:13:36 +1000

    0.0.4

--- locators.py
+++ locators.py
@@ -1,7 +1,10 @@
+from selenium.webdriver.common.by import By
+
 class Locator:
-    locator2 = ('XPATH', '//*[@id="js-contribution-activity"]/div[2]/div/div/div[2]/details/summary/span[2]/span[2]')
-    locator3 = ('XPATH', '//span[@class="Details-content--closed float-right"]')
-    locator1 = ('XPATH', '//span[@class="d-inline-block float-right"]')
+    locator2 = (By.XPATH, '//*[@id="js-contribution-activity"]/div[2]/div/div/div[2]/details/summary/span[2]/span[2]')
+    locator3 = (By.XPATH, '//span[@class="Details-content--closed float-right"]')
+    locator4 = (By.XPATH, '//span[@class="d-inline-block float-right"]')
+    locator1 = (By.XPATH, '//main')
 
 
 ##<span class="Details-content--closed float-right" data-hydro-click="{&quot;event_type&quot;:&quot;user_profile.click&quot;,&quot;payload&quot;:{&quot;profile_user_id&quot;:51861420,&quot;target&quot;:&quot;TIMELINE_CATEGORY_ROLLUP_EXPAND&quot;,&quot;user_id&quot;:177808467,&quot;originating_url&quot;:&quot;https://github.com/irisqul?action=show&amp;controller=profiles&amp;tab=contributions&amp;user_id=irisqul&quot;}}" data-hydro-click-hmac="f0ff594a7d194fc932aff610dfe0b41d7195d4fc435f30b5e22fb7f064b719d2"></span>
--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from locators import Locator
 from locators import Where
 from selenium import webdriver
 
+from selenium.webdriver.common.by import By
+
+
 
 def test_login(ddriver):
     driver = ddriver
     driver.get(Where.main_pass)
-    print('\n',Locator.locator1[0],Locator.locator1[1])
-    elms = driver.find_elements(Locator.locator1[0],Locator.locator1[1])
+    print('\n',Locator.locator3[0],Locator.locator3[1])
+    elms = driver.find_elements(Locator.locator3[0],Locator.locator3[1])
+    #elms = driver.find_elements(By.XPATH, '//main')
+    print(len(elms))
     for elm in elms:
         print(elm.get_attribute('class'))
     print(driver.current_url)
